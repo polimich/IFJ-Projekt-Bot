99233cb2d99e3d01a3445b652f0ca659bc53f66e
Prokop <prokop.schield@protonmail.com>
2022-10-05 17:42:48 +0200

    fix(src/modules/loader.ts): load from modules dir

--- src/modules/loader.ts
+++ src/modules/loader.ts
@@ -7,7 +7,10 @@ export default async function loader(bot: Client<true>, symbol?: symbol) {
 	if (symbol !== symbol_no_recursion) {
 		for (const module_name of await fs.promises.readdir(__dirname)) {
 			if (module_name.endsWith(".js")) {
-				require(module_name).default(bot, symbol_no_recursion);
+				require(__dirname + "/" + module_name).default(
+					bot,
+					symbol_no_recursion
+				);
 			}
 		}
 	}
