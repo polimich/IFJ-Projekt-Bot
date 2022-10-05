98987559120352b913e430c99dc8d0312bbd42e9
Prokop <prokop.schield@protonmail.com>
2022-10-05 15:59:26 +0200

    feat(src/modules/loader.ts): add module loader

--- src/cli.ts
+++ src/cli.ts
@@ -3,5 +3,8 @@
 import argv from "@prokopschield/argv";
 
 import { bot } from "./bot";
+import loader from "./modules/loader";
 
 bot.login(argv.expectMutate(["token"]).token);
+
+bot.on("ready", (bot) => loader(bot));
---
+++ src/modules/loader.ts
@@ -0,0 +1,14 @@
+import { Client } from "discord.js";
+import fs from "fs";
+
+const symbol_no_recursion = Symbol("loader-no-recursion");
+
+export default async function loader(bot: Client<true>, symbol?: symbol) {
+	if (symbol !== symbol_no_recursion) {
+		for (const module_name of await fs.promises.readdir(__dirname)) {
+			if (module_name.endsWith(".js")) {
+				require(module_name).default(bot, symbol_no_recursion);
+			}
+		}
+	}
+}
